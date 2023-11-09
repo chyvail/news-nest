8afa5d17622e0fa589b34fa031b55dfa75aed6e1
joshua chivile <joshuachivile@gmail.com>
2023-11-09 06:10:09 +0300

    Show Stories Count on Page

--- src/Components/NewsStories.js
+++ src/Components/NewsStories.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useContext } from "react";
 import Card from "./Card";
+import { NewsContext } from "../Contexts/NewsContext";
 
-export default function NewsStories({ api, clientKey }) {
+export default function NewsStories() {
+  const { newsData } = useContext(NewsContext);
   return (
     <div className="mt-3 containerlg">
-      <h5 id="stories-count"></h5>
+      <h5 id="stories-count">Latest Stories ({newsData.length})</h5>
       <div id="stories" className="row g-2">
         <Card />
       </div>
