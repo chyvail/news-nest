a12770bd9b15a6a2f38ede492b220fc634bd572a
joshua chivile <joshuachivile@gmail.com>
2023-11-09 06:18:56 +0300

    Add key prop and used ClassName

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -6,10 +6,13 @@ export default function Card() {
   return (
     <>
       {newsData.map((news) => (
-        <div class="col-sm-3 d-flex flex-column custom-md-size">
-          <div class="card flex-fill">
+        <div
+          key={news.title}
+          className="col-sm-3 d-flex flex-column custom-md-size"
+        >
+          <div className="card flex-fill">
             <img
-              class="card-img-top"
+              className="card-img-top"
               src={
                 news.image === null
                   ? "https://img.freepik.com/free-photo/top-view-old-french-newspaper-pieces_23-2149318857.jpg?w=1800&t=st=1697118921~exp=1697119521~hmac=a7cbfebd6578b839daf35fd4850eec18106aa1152927e8f33305175ef357cdaf"
@@ -17,23 +20,25 @@ export default function Card() {
               }
               alt={news.author}
             />
-            <div class="card-body">
+            <div className="card-body">
               <h6>{news.title}</h6>
-              <p class="card-text">{news.description}</p>
-              <div class="card-author">
-                <p class="card-text mt-2">{news.published_at.slice(0, 10)}</p>
-                <p class="card-text mt-2">~ {news.author}</p>
+              <p className="card-text">{news.description}</p>
+              <div className="card-author">
+                <p className="card-text mt-2">
+                  {news.published_at.slice(0, 10)}
+                </p>
+                <p className="card-text mt-2">~ {news.author}</p>
               </div>
             </div>
             <hr />
-            <div class="icons">
-              <div id="like" class="like">
-                <i id="like-icon" class="fa-regular fa-thumbs-up"></i>
-                <p class="count">0</p>
+            <div className="icons">
+              <div id="like" className="like">
+                <i id="like-icon" className="fa-regular fa-thumbs-up"></i>
+                <p className="count">0</p>
               </div>
-              <div class="bookmark">
-                <i id="like-icon" class="fa-regular fa-bookmark"></i>
-                <p class="count">0</p>
+              <div className="bookmark">
+                <i id="like-icon" className="fa-regular fa-bookmark"></i>
+                <p className="count">0</p>
               </div>
             </div>
           </div>
