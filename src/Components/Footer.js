d8bbbd7b76f9cdf829132572e232915b2bdcf218
joshua chivile <joshuachivile@gmail.com>
2023-11-09 06:03:01 +0300

    Added Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      Designed and built with ❤️ by{" "}
+      <a href="https://github.com/chyvail" target="_blank">
+        Josh
+      </a>
+    </footer>
+  );
 }
