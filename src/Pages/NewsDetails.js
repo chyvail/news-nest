b8cc8f232f566028de24e7216cf76977d9ca300d
joshua chivile <joshuachivile@gmail.com>
2023-11-10 09:42:03 +0300

    Patch working on newsDetails

--- src/Pages/NewsDetails.js
+++ src/Pages/NewsDetails.js
@@ -1,18 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { NewsContext } from "../Contexts/NewsContext";
 import { useParams, Link, useNavigate } from "react-router-dom";
 
 export default function NewsDetails() {
   const { id } = useParams();
   const navigate = useNavigate();
+  // Context
   const { newsData, defaultImage } = useContext(NewsContext);
+
+  // State
+  const [comments, setComments] = useState([]);
+
+  // Form State
+  const [commentText, setCommentText] = useState("");
 
   // Find the specific news item based on the id parameter
   const newsItem = newsData[parseInt(id, 10)];
-  // Handle the case where the news item is not found
-  if (!newsItem) {
-    return <div>News not found</div>;
-  }
+
+  const handleCommentSubmit = (event) => {
+    event.preventDefault();
+    fetch(`http://localhost:8000/comments/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ commentsList: [...comments, commentText] }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/comments/${id}`)
+      .then((res) => res.json())
+      .then((data) => setComments(data.commentsList || []));
+  });
 
   return (
     <>
@@ -36,7 +58,56 @@ export default function NewsDetails() {
               alt={newsItem.author}
             />
           </div>
-          <div className="icons mt-3">
+
+          <div className="text-center mt-3">
+            <a
+              type="button"
+              className="btn original-link"
+              href={newsItem.url}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Go to Original Link
+            </a>
+          </div>
+
+          {/* Comments Section */}
+
+          <div className="comments">
+            <h5>Comments</h5>
+          </div>
+          <div className="mt-3">
+            {comments.map((comment) => (
+              <p key={comment} className="individual-comments mt-1">
+                {comment}
+              </p>
+            ))}
+          </div>
+
+          {/* Form Section */}
+          <form className="mt-3" onSubmit={handleCommentSubmit}>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Make A Comment Here
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="commentText"
+                aria-describedby="emailHelp"
+                value={commentText}
+                onChange={(event) => setCommentText(event.target.value)}
+              />
+              <div id="textHelp" className="form-text">
+                What are you thinking about
+              </div>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit Comment
+            </button>
+          </form>
+
+          <div className="icons mt-3 news-icons">
             <div id="like" className="like">
               <i className="fa-regular fa-clock"></i>
               <p className="count">{newsItem.published_at.slice(0, 10)}</p>
@@ -45,17 +116,6 @@ export default function NewsDetails() {
               <i className="fa-regular fa-user"></i>
               <p className="count">~ {newsItem.author}</p>
             </div>
-          </div>
-          <div className="text-center">
-            <a
-              type="button"
-              class="btn original-link"
-              href={newsItem.url}
-              target="_blank"
-              rel="noreferrer"
-            >
-              Go to Original Link
-            </a>
           </div>
         </div>
       </div>
